ab2f6bd4b4270a22a83414f5cb9c81a7a93a334e
AmalBut <136389347+AmalBut@users.noreply.github.com>
2024-12-10 18:44:52 +0200

    Add files via upload

---
+++ LinkedStack/Program.cs
@@ -0,0 +1,163 @@
+﻿using System;
+namespace LinkedStack
+{
+
+   class StackDrive
+    {
+        static void Main(string[] args)
+        {
+            Stack<int> stack = new Stack<int>();
+            try
+            {
+                Console.WriteLine("Enter expression to check if balanced or not");
+                string exp= Console.ReadLine();
+                if (AreBalanced(exp))
+                {
+                    Console.WriteLine("The expression is balanced");
+                }
+                else
+                {
+                    Console.WriteLine("The expression is NOT balanced");
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        public static bool AreBalanced(string exp)
+        {
+            Stack<char> stack = new Stack<char>();
+            bool valid = true ;
+            for(int i=0; i<exp.Length; i++)
+            {
+                if (exp[i] == '{' || exp[i] == '(' || exp[i] == '[')
+                {
+                    stack.Push(exp[i]);
+                }
+               else if (exp[i] == '}' || exp[i] == ')' || exp[i] == ']')
+                {
+                    if (stack.IsEmpty() || !ArePair(stack.getTop(), exp[i]))
+                    {
+                       
+                             valid=false;
+                    }
+                    else
+                    {
+                        stack.Pop();
+                    }
+                }
+                
+            }
+            if (!stack.IsEmpty())
+            {
+                valid= false;
+            }
+          
+            return valid;
+            
+        }
+
+        public static bool ArePair(char open, char close)
+        {
+            if (open == '(' && close == ')')
+                return true;
+            else if (open == '{' && close == '}')
+                return true;
+            else if (open == '[' && close == ']')
+                return true;
+            else
+                return false;
+        }
+
+        /*public static bool AreBalanced(string exp)
+        {
+
+            Stack<char> stack = new Stack<char>();
+            bool valid = true;
+
+             for(int i = 0; i < exp.Length; i++)
+             {
+                 if (exp[i]=='{' || exp[i]=='(' || exp[i] == '[')
+                 {
+                     stack.Push(exp[i]);
+                 }
+
+                     if (exp[i] == '}')
+                     {
+                     if (!stack.IsEmpty())
+                     {
+
+                         if (stack.PopTop() == '{')
+                         {
+                             valid = true;
+                         }
+                         else
+                         {
+                             valid = false;
+                         }
+                     }
+                     else
+                     {
+                         valid = false;
+                     }
+
+                     }
+                     else if (exp[i] == ']')
+                     {
+                     if (!stack.IsEmpty())
+                     {
+                         if (stack.PopTop() == '[')
+                         {
+                             valid = true;
+                         }
+                         else
+                         {
+                             valid = false;
+                         }
+                     }
+                     else
+                     {
+                         valid = false;
+                     }
+
+                 }
+                     else if (exp[i] == ')')
+                     {
+                     if (!stack.IsEmpty())
+                     {
+
+                         if (stack.PopTop() == '(')
+                         {
+                             valid = true;
+                         }
+                         else
+                         {
+                             valid = false;
+                         }
+                     }
+                     else
+                     {
+                         valid = false;
+                     }
+
+                 }
+                 }
+
+
+             if (!stack.IsEmpty())
+             {
+                 valid = false;
+             }
+             return valid;
+
+        }*/
+
+
+
+
+    }
+
+}
+
---
+++ LinkedStack/Stack.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedStack
+{
+    public class Node<T>
+    {
+        public T item;
+        public Node<T> next;
+
+        public Node(T item)
+        {
+            this.item = item;
+        }
+
+    }
+    public class Stack<T>
+    {
+        public Node<T> stackTop;
+        int count;
+        public Stack()
+        {
+            stackTop = null;
+            count = 0;
+        }
+
+        public void Push(T item)
+        {
+            Node<T> newNode = new Node<T>(item);
+            if (newNode == null)
+            {
+                throw new InvalidOperationException("Stack push cannot alloctae memory");
+            }
+            newNode.next = stackTop;
+            stackTop = newNode;
+            count++;
+        }
+
+        public void Pop()
+        {
+            if (IsEmpty())
+                throw new InvalidOperationException("Stack is empty.");
+
+            Node<T> temp = stackTop;
+            stackTop = stackTop.next;
+            temp.next = null;
+            count--;
+
+
+        }
+
+        public T PopTop()
+        {
+
+            if (IsEmpty())
+                throw new InvalidOperationException("Stack is empty.");
+
+            T top = stackTop.item;
+            Node<T> temp = stackTop;
+            stackTop = stackTop.next;
+            temp.next = null;
+
+            count--;
+            return top;
+
+        }
+        public T getTop()
+        {
+            if (IsEmpty())
+            {
+                throw new InvalidOperationException("Stack is empty.");
+
+            }
+
+            return stackTop.item;
+        }
+
+        public bool IsEmpty()
+        {
+            return stackTop == null;
+        }
+
+        public void Display()
+        {
+            Node<T> current = stackTop;
+
+            while (current != null)
+            {
+                Console.WriteLine(current.item);
+                current = current.next;
+            }
+        }
+        public int Count()
+        {
+            return count;
+        }
+
+
+    }
+}
